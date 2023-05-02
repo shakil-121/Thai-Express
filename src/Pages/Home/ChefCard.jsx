227d73f3a673bbb2297da3c06430e0a168c20cbf
shakil-121 <shahadatshakil76@gmail.com>
2023-05-02 06:15:31 -0700

    chef data fetch done for chef card

--- src/Pages/Home/ChefCard.jsx
+++ src/Pages/Home/ChefCard.jsx
@@ -15,7 +15,7 @@ const ChefCard = ({ chef }) => {
             <p>Total Recipe: {Numbersofrecipes}</p>
           </div>
         </div>
-        <div className="card-body border-top bg-dark d-flex">
+        <div className="card-body border-top bg-dark d-flex justify-content-between">
           <div className="d-flex align-items-center">
              <p className="mb-0 text-light">{TotalLikes}</p>
              <BiLike className="text-primary"></BiLike>
